cf4dc84f5d39b5cf9baf93cce83033a8decab915
keren Ramos <keren.ramoss@gmail.com>
2022-12-07 12:08:39 -0600

    proyecto terminado

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
+// constantes 
 const plus = document.getElementById('plus');
 const less = document.getElementById('less');
 const etiqueta = document.getElementById('etiqueta');
 const orden = document.getElementById('orden');
+const precio = document.getElementById("precio");
+const envio = document.getElementById("envio");
+const total = document.getElementById("total");
 
+// escucha cuando hace click al mas
 plus.addEventListener('click',(e)=>{
 let contadorActual =parseInt(etiqueta.textContent,10);
  let contador = contadorActual+1;
@@ -10,7 +15,7 @@ let contadorActual =parseInt(etiqueta.textContent,10);
 
  actualizarLista(contadorActual,contador);
 });
-
+// escucha cuando hace click al mas
 less.addEventListener('click',(e)=>{
     let contadorActual =parseInt(etiqueta.textContent,10);
     let contador = (contadorActual-1)<0?0:(contadorActual-1);
@@ -18,10 +23,9 @@ less.addEventListener('click',(e)=>{
     actualizarLista(contadorActual,contador);
    });
 
-
+// agrega o quita producto de la lista
    function actualizarLista(contadorActual,contador){
-
-    const html = ` <li>
+    const html = ` <li class="orden__informacion">
     <p>camiseta Negra</p>
     <p>L<span>1000.00</span></p>
     </li>`;
@@ -31,15 +35,19 @@ less.addEventListener('click',(e)=>{
     }else if(contadorActual>contador){
         orden.querySelector('li').remove();
     }
-
-    
+    actualizarTotal(contador);
    }
 
+// actualiza el total al agregar y quitar un producto
+   function actualizarTotal(contador){
 
+    let subtotal = contador * parseInt(precio.textContent) + parseInt(envio.textContent); 
 
-
-
-
+    if(contador==0){
+        subtotal =0.00;
+    }
+    total.textContent = subtotal.toFixed(2);
+   }
 
 
 // contructor de objeto persona
